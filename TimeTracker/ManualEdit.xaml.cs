616cc1cf83b7cf73bfd4eacdee065c3a26b2309b
Robin Weitzel <robin.weitzel@fim-rc.de>
2018-12-03 08:25:31 +0100

    Removed unneeded classes and fixed ManualEdit.

--- TimeTracker/ManualEdit.xaml.cs
+++ TimeTracker/ManualEdit.xaml.cs
@@ -22,7 +22,6 @@ namespace TimeTracker
     public partial class ManualEdit : Window
     {
         List<Helper.Activity> Activities;
-        List<Helper.Activity> Activities_Duplicate;
         public ManualEdit()
         {
             InitializeComponent();
@@ -37,8 +36,7 @@ namespace TimeTracker
                 var csv = new CsvReader(tr);
                 var records = csv.GetRecords<Helper.Activity>();
 
-                Activities = records.OrderByDescending(aa => aa.To).Take(20).ToList();
-                Activities_Duplicate = records.OrderByDescending(aa => aa.To).Take(20).ToList();
+                Activities = records.OrderByDescending(aa => aa.To).ToList();
                 DataGrid.ItemsSource = Activities;
             }
         }
@@ -50,33 +48,12 @@ namespace TimeTracker
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            /*using (TextWriter tw = new StreamWriter(Variables.activityPath))
+            using (TextWriter tw = new StreamWriter(Variables.activityPath))
             {
                 var csv = new CsvWriter(tw);
-                foreach (Helper.Activity activity_duplicate in Activities_Duplicate)
-                {
-                    Helper.Activity activity = Activities.Where(aa => aa.Id == activity_duplicate.id).FirstOrDefault();
-                    activity_active activity_db = db.activity_active.Find(activity_duplicate.id);
-                    if (activity == null)
-                    {
-                        if(activity_db != null)
-                            db.activity_active.Remove(db.activity_active.Find(activity_duplicate.id));
-                    }
-                    else
-                    {
-                        if(activity_db != null)
-                        {
-                            activity_db.name = activity.name;
-                            activity_db.from = activity.from;
-                            activity_db.to = activity.to;
-                        }
-                    }
-
-                    db.SaveChanges();
-                }
-
-                loadData();
-            }*/
+                csv.WriteRecords(Activities);
+            }
+            loadData();
         }
     }
 }
