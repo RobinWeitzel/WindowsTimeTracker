5dec50f3b2958c510f3ac9587eca7a2dae45cce6
Robin Weitzel <robin.weitzel@fim-rc.de>
2019-11-16 13:15:11 +0100

    added JS file to act as replacement for C# when page is viewed in browser

--- js/ViewBugreport.js
+++ js/ViewBugreport.js
@@ -8,22 +8,24 @@ TSFRepository.registerComponent(class ViewBugreport extends TSFComponent {
     sendReport(e) {
         const escapeHtml = unsafe => {
             return unsafe
-                 .replace(/&/g, "&amp;")
-                 .replace(/</g, "&lt;")
-                 .replace(/>/g, "&gt;")
-                 .replace(/"/g, "&quot;")
-                 .replace(/'/g, "&#039;")
-                 .replace(/\n/g, "%0A")
-                 .replace(/ /g, "%20");
-         }
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#039;")
+                .replace(/\n/g, "%0A")
+                .replace(/ /g, "%20");
+        }
 
-        const xmlHttp = new XMLHttpRequest();
-        /*xmlHttp.onreadystatechange = function() { 
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-                callback(xmlHttp.responseText);
-        }*/
-        xmlHttp.open("GET", `https://hooks.zapier.com/hooks/catch/3466553/obv012p/?issue_body=${escapeHtml(this.state.report)}`, true); // true for asynchronous 
-        xmlHttp.send(null);
+        if (CefSharp.blockReport !== true) {
+            const xmlHttp = new XMLHttpRequest();
+            /*xmlHttp.onreadystatechange = function() { 
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                    callback(xmlHttp.responseText);
+            }*/
+            xmlHttp.open("GET", `https://hooks.zapier.com/hooks/catch/3466553/obv012p/?issue_body=${escapeHtml(this.state.report)}`, true); // true for asynchronous 
+            xmlHttp.send(null);
+        }
         this.state.report = "";
     }
 });
